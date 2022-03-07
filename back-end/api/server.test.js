83b8a6446f382a138e32f0cf8509cd714b713423
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-03-07 14:08:11 -0500

    created tests for ping and posts endpoints, endpoints pass the inital 200 code test

---
+++ back-end/api/server.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest")
+const server = require("./server")
+
+describe('endpoints', () => {
+  describe("[GET] /api/ping", () => {
+    it("responds with a 200", async () => {
+      const res = await request(server).get("/api/ping")
+      expect(res.status).toBe(200)
+    })
+  })
+  describe("[GET] /api/posts", () => {
+    it("responds with a 200", async () => {
+      const res = await request(server).get("/api/posts")
+    })
+  })
+})
